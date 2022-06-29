392b099228c514d4516a0ce63d33f0e045bdbae8
knafil12201 <ryo01251019@akane.waseda.jp>
2022-06-29 15:06:01 +0900

    Add EditView

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,8 @@
 */
 
 Route::get('/', 'PostController@index');
+Route::post('/posts', 'PostController@store');
 Route::get('/posts/create', 'PostController@create');
 Route::get('/posts/{post}', 'PostController@show');
-
-Route::post('/posts', 'PostController@store');
+Route::put('/posts/{post}', 'PostController@update');
+Route::get('/posts/{post}/edit', 'PostController@edit');
